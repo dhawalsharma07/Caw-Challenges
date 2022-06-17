09da45e843183a42fea156ccde069ec6b046adb9
Dhawal Sharma <dhawalsharma2301@gmail.com>
2022-06-17 12:08:07 +0530

    Files added for Piano Challenge

---
+++ challenge-3/app.js
@@ -0,0 +1,22 @@
+
+ const playsound = (filepath) => {
+     
+    return function() {
+   
+        return new Audio(filepath).play();
+    }
+          
+ }
+const sound=()=>{
+
+    let anchortags=document.querySelectorAll('a');
+    for(let index=0; index < anchortags.length; index++){
+
+        anchortags[index].addEventListener('click',playsound(getPath(index+1)));
+    }
+}
+const getPath = (index) => {
+    return `./audio/key-${index}.mp3`;
+}
+
+sound();
