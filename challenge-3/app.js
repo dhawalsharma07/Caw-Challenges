2ef8cf4afcb9cbe5c494992eeeb3102a12f5d73b
Dhawal Sharma <dhawalsharma2301@gmail.com>
2022-06-17 12:25:09 +0530

    Changes in app.js files

--- challenge-3/app.js
+++ challenge-3/app.js
@@ -1,22 +1,15 @@
-
- const playsound = (filepath) => {
-     
+const playsound = (filepath) => { 
     return function() {
-   
         return new Audio(filepath).play();
-    }
-          
+    }    
  }
 const sound=()=>{
-
     let anchortags=document.querySelectorAll('a');
     for(let index=0; index < anchortags.length; index++){
-
         anchortags[index].addEventListener('click',playsound(getPath(index+1)));
     }
 }
-const getPath = (index) => {
+const getPath=(index)=>{
     return `./audio/key-${index}.mp3`;
 }
-
 sound();
